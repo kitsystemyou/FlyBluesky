de9946d27a2a8fe3eb4631701b8e003f4dd70555
kitsystemyou <kitsystemyou@gmail.com>
2024-02-08 01:02:56 +0900

    fix time

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 from atproto import Client
 import os
 import time
+from datetime import datetime
+from zoneinfo import ZoneInfo
 
 client = Client(base_url='https://bsky.social')
 client.login(os.environ['MAIL'],
              os.environ['PASS'])
 
-nowtime = time.strftime('%Y-%M-%D %H:%M:%S', time.localtime(time.time()))
+tl = time.localtime(time.time())
+
+nowtime = datetime.now(ZoneInfo("Asia/Tokyo")).strftime('%Y-%M-%D %H:%M:%S')
 post: str = "now! " + nowtime
 print("post: ", post)
 post = client.send_post(post)
